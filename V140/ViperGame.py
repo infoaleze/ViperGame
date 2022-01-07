3468fdc24acaa9e87d0cfa969abe51186565fb29
Info @ Lèze <infoaleze@users.noreply.github.com>
2022-01-07 18:06:49 +0100

    V140 - Exercice

---
+++ V140/ViperGame.py
@@ -0,0 +1,36 @@
+import VGGfx
+import VGNetLib
+import VGCore
+import os
+
+# ==================================================================
+# ==                INITIALISATION DE PYGAME                      ==
+# ==================================================================
+
+
+baseDir = os.path.dirname(os.path.realpath(__file__))
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#!!   AJouter la création des Classe            !!
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Initialisation
+
+
+
+# ==========================================================================
+# ==               B O U C L E   P R I N C I P A L E
+# ==========================================================================
+
+run = True
+
+while run:
+
+    # Reception des messages Réseau
+    #VGNetLib.ReceptionMessage()
+
+    # Actualisation de l'affichage
+
+    pass
+    #run = myGfx.render()
+
